759c0dbb0032d26f12e697d5689fd9a2b118c46a
Maxim <probmak@gmail.com>
2014-08-30 13:41:38 +0400

    first commit

---
+++ Assets/src/HeroControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroControl : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public void Move ()
+	{
+		var pos = Input.mousePosition;
+		Debug.Log (pos);
+	}
+}	
---
+++ Assets/src/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using AssemblyCSharp;
+
+public class Menu : MonoBehaviour 
+{
+	public Text Name;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void LoadLevel() 
+	{
+		if (Name != null) 
+		{
+			Debug.Log("CHECK");
+			Debug.Log(Name.text);
+			Settings.PLAYER_NAME = Name.text;
+		}
+		Debug.Log("welcome " + Settings.PLAYER_NAME.ToString());
+		Settings.GAME_TYPE = 0;
+		Application.LoadLevel (1);
+	}
+}
