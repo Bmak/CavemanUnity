74232ce4c572266692922c978f11f492b3ca0ef5
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2014-09-11 18:53:16 +0400

    новая версия.
    не надо строить логику игры на гуи. гуи для гуи должно быть.
    слежение камеры , отделил гуи .

---
+++ Assets/MokarController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MokarController : MonoBehaviour 
+{
+    int speed = 10;
+
+    float vx = 0;
+    float vy = 0;
+    float targetX = 0;
+    float targetY = 0;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+            print("move");
+	        Move();
+	    }
+
+        var pos = transform.position;
+        pos.x = pos.x + vx * Time.deltaTime;
+        pos.y = pos.y + vy * Time.deltaTime;
+        transform.position = pos;
+	}
+
+    public void Move()
+    {
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+
+        Debug.Log(pos);
+        targetX = pos.x;
+        targetY = pos.y;
+
+        var heroPos = transform.position;
+
+        float dx = targetX - heroPos.x;
+        float dy = targetY - heroPos.y;
+        float d = Mathf.Sqrt(dx * dx + dy * dy);
+
+        if (d != 0)
+        {
+            vx = (dx / d) * speed;
+            vy = (dy / d) * speed;
+        }
+    }
+}
---
+++ Assets/src/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform TargetPosition;
+    public float smoothTime = 0.3F;
+
+    private Vector3 velocity = Vector3.zero;
+	
+	void FixedUpdate ()
+	{
+	    if (TargetPosition != null)
+	    {
+            var smoothPosHero = Vector3.SmoothDamp(transform.position, TargetPosition.position, ref velocity, smoothTime);    
+            // todo -10 
+            transform.position = new Vector3(smoothPosHero.x, smoothPosHero.y, -10);
+	    }
+	}
+}
